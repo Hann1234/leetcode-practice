d29e745ff7ca2dbb943effe2623b0c725ce87de9
Hann1234 <christopherjhannigan@gmail.com>
2021-11-26 16:27:00 -0600

    added clearer notes

--- singleNumber.js
+++ singleNumber.js
@@ -1,9 +1,9 @@
 let singleNumber = (nums) => {
 
-  const hashTable = {}; //creat a hash table to keep track of how many times a number appears in our array
+  const hashTable = {}; //create a hash table as an object to keep track of how many times a number appears in our nums array
                         //{1000: 5, 3: 200, 234: 2}
   for (const num of nums) {
-      hashTable[num] = hashTable[num] + 1 || 1; //creates our hash table object values
+      hashTable[num] = hashTable[num] + 1 || 1; //creates our hash table object properties
   }
 
   for (const key in hashTable) {
