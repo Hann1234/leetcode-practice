c6f30c5639a7fcbd28e1b5de157e4fa8fe0fe71f
Hann1234 <christopherjhannigan@gmail.com>
2021-11-26 16:32:31 -0600

    added increment instead of just + 1

--- singleNumber.js
+++ singleNumber.js
@@ -3,7 +3,7 @@ let singleNumber = (nums) => {
   const hashTable = {}; //create a hash table as an object to keep track of how many times a number appears in our nums array
                         //{1000: 5, 3: 200, 234: 2}
   for (const num of nums) {
-      hashTable[num] = hashTable[num] + 1 || 1; //creates our hash table object properties
+      hashTable[num] = hashTable[num] ++ || 1; //creates our hash table object properties or increments 1 if number already exists
   }
 
   for (const key in hashTable) {
