3649037fdc224a0b3d1ae25cc39a15892e6643fa
Hann1234 <christopherjhannigan@gmail.com>
2021-11-26 16:07:31 -0600

    created a hash table to store values as properties

--- singleNumber.js
+++ singleNumber.js
@@ -1,7 +1,8 @@
 let singleNumber = (nums) => {
-  const hashTable = {}; //creat a hash table to keep track of how many times a number appears in our array
 
+  const hashTable = {}; //creat a hash table to keep track of how many times a number appears in our array
+                        //{1000: 5, 3: 200, 234: 2}
   for (const num of nums) {
-      
+      hashTable[num] = hashTable[num] + 1 || 1; //creates our hash table object values
   }
 };
